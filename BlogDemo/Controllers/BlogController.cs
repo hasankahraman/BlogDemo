af3ac67893f2d571aed47a55562448f40b291e98
HASAN KAHRAMAN <hasankahraman45@outlook.com>
2023-10-11 17:22:38 +0300

    blog index page has been created

--- BlogDemo/Controllers/BlogController.cs
+++ BlogDemo/Controllers/BlogController.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BlogDemo.Controllers
 {
     public class BlogController : Controller
     {
+		BlogManager manager = new BlogManager(new EFBlogDAL());
         public IActionResult Index()
         {
-            return View();
+			var blogs = manager.GetAll();
+            return View(blogs);
         }
-    }
+
+        public PartialViewResult PVHeader()
+        {
+            return PartialView();
+        }
+		public PartialViewResult PVFooter()
+		{
+			return PartialView();
+		}
+		public PartialViewResult PvSign()
+		{
+			return PartialView();
+		}
+		public PartialViewResult PVSocial()
+		{
+			return PartialView();
+		}
+	}
 }
---
+++ BussinessLayer/Concrete/BlogManager.cs
@@ -0,0 +1,46 @@
+﻿using BussinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.Concrete
+{
+	public class BlogManager : IBlogService
+	{
+		IBlogDAL _blogDAL;
+
+		public BlogManager(IBlogDAL blogDAL)
+		{
+			_blogDAL = blogDAL;
+		}
+
+		public void Add(Blog blog)
+		{
+			_blogDAL.Add(blog);
+		}
+
+		public void Delete(Blog blog)
+		{
+			_blogDAL.Delete(blog);
+		}
+
+		public List<Blog> GetAll()
+		{
+			return _blogDAL.Get();
+		}
+
+		public Blog GetById(int id)
+		{
+			return _blogDAL.GetById(id);
+		}
+
+		public void Update(Blog blog)
+		{
+			_blogDAL.Update(blog);
+		}
+	}
+}
