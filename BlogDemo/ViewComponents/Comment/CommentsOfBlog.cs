1e4d2feef2b9f71abd4f5977bcffc1ae47e97c26
HASAN KAHRAMAN <hasankahraman45@outlook.com>
2023-10-17 00:20:38 +0300

    blog details page is still continue

--- BlogDemo/ViewComponents/Comment/CommentsOfBlog.cs
+++ BlogDemo/ViewComponents/Comment/CommentsOfBlog.cs
@@ -2,15 +2,19 @@
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BlogDemo.ViewComponents
+namespace BlogDemo.ViewComponents.Comment
 {
 	public class CommentsOfBlog : ViewComponent
 	{
 		CommentManager manager = new CommentManager(new EFCommentDAL());
 
-		public IViewComponentResult Invoke()
+		public IViewComponentResult Invoke(int blogId)
 		{
-			var comments = manager.GetAll(1);
+			var comments = manager.GetAll(blogId);
+			if (comments.Count == 0)
+			{
+				ViewBag.nocomment = "No comments has been written. Be the first one to comment.";
+			}
 			return View(comments);
 		}
 	}
