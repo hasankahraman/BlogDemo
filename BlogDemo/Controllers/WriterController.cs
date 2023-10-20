ce7b662fb694be7a4f94fa5845a0cee2213c734b
HASAN KAHRAMAN <hasankahraman45@outlook.com>
2023-10-20 17:22:34 +0300

    auth

---
+++ BlogDemo/Controllers/WriterController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogDemo.Controllers
+{
+	public class WriterController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult WriterProfile()
+		{
+			return View();
+		}
+	}
+}
