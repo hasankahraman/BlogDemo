089905ecded768a68d219076ca1546ca26821ef9
HASAN KAHRAMAN <hasankahraman45@outlook.com>
2023-11-07 17:26:24 +0300

    messages

---
+++ BlogDemo/Controllers/MessageController.cs
@@ -0,0 +1,24 @@
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogDemo.Controllers
+{
+    public class MessageController : Controller
+    {
+        MessageManager manager = new MessageManager(new EFMessageDAL());
+        public IActionResult Inbox()
+        {
+            int writerId = 2;
+            var messages = manager.GetInboxByWriter(writerId);
+            return View(messages);
+        }
+        public IActionResult Sentbox()
+        {
+            int writerId = 2;
+            var messages = manager.GetSentboxByWriter(writerId);
+            return View(messages);
+        }
+
+    }
+}
