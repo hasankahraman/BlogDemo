6c849e5e5acc1c2ee5220206a212e191353ffd00
HASAN KAHRAMAN <hasankahraman45@outlook.com>
2023-11-14 23:37:10 +0300

    100 Statistics 2

---
+++ BlogDemo/Areas/Admin/ViewComponents/Statistic/StatisticsMain.cs
@@ -0,0 +1,20 @@
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogDemo.Areas.Admin.ViewComponents.Statistic
+{
+    public class StatisticsMain: ViewComponent
+    {
+        BlogManager blogManager = new BlogManager(new EFBlogDAL());
+        ContactManager contactManager = new ContactManager(new EFContactDAL());
+        CommentManager commentManager = new CommentManager(new EFCommentDAL());
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.TotalBlogs = blogManager.GetAll().Count;
+            ViewBag.TotalContact = contactManager.GetAll().Count;
+            ViewBag.TotalComments = commentManager.GetAll().Count;
+            return View();
+        }
+    }
+}
