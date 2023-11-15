dd518f5e48873f1264f7058e4ecde1e3081fb798
HASAN KAHRAMAN <hasankahraman45@outlook.com>
2023-11-15 17:24:20 +0300

    stat lower part 2 done

---
+++ BlogDemo/Areas/Admin/ViewComponents/Statistic/StatisticsLower.cs
@@ -0,0 +1,21 @@
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace BlogDemo.Areas.Admin.ViewComponents.Statistic
+{
+    public class StatisticsLower : ViewComponent
+    {
+        BlogManager blogManager = new BlogManager(new EFBlogDAL());
+        ContactManager contactManager = new ContactManager(new EFContactDAL());
+        CommentManager commentManager = new CommentManager(new EFCommentDAL());
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.LatestBlogName = blogManager.GetLatest3Blogs().OrderByDescending(x => x.CreatedAt).FirstOrDefault().Title;
+            ViewBag.LatestBlogDesc = blogManager.GetLatest3Blogs().OrderByDescending(x => x.CreatedAt).FirstOrDefault().Content.Substring(0,25);
+            ViewBag.TotalComments = commentManager.GetAll().Count;
+            return View();
+        }
+    }
+}
