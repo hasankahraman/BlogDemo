856eeeb25e2236aad0e616aa3c821f02b2efa075
HASAN KAHRAMAN <hasankahraman45@outlook.com>
2023-12-08 15:06:16 +0300

    jwt done

---
+++ BlogDemo.JWT/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using BlogDemo.JWT.DAL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogDemo.JWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult Login()
+        {
+            return Created("", new BuildToken().CreateToken());
+        }
+
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult Homepage()
+        {
+            return Ok("Successfully logined to homepage");
+        }
+    }
+}
